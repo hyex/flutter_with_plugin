a033fb9dc378c0496d06d9bc48f6ea2c4702c6bb
hyex <hyexkim@gmail.com>
2021-11-28 15:37:33 +0900

    :sparkles: 안드로이드 버전 가져오기
    - https://here4you.tistory.com/260
    - MainActivity.java 의 package 이름이 java 하위 경로와 일치해야 함 (안그럼 앱 시작을 못함)

--- android/app/src/main/java/com/example/my_flutter/MainActivity.java
+++ android/app/src/main/java/com/example/my_flutter/MainActivity.java
@@ -1,7 +1,28 @@
 package com.example.my_flutter;
 
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-}
 
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        final MethodChannel channel = new MethodChannel(flutterEngine.getDartExecutor(), "com.example.methodchanneltest");
+        channel.setMethodCallHandler(handler);
+    }
+
+    private MethodChannel.MethodCallHandler handler = (methodCall, result) -> {
+        if (methodCall.method.equals("getPlatformVersion")) {
+            result.success("Android Version: " + Build.VERSION.RELEASE);
+        } else {
+            result.notImplemented();
+        }
+    };
+}
